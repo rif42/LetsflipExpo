ec13c4ba0d09a6f098fade9d0f876705f183616f
rifky <ee.guy.42@gmail.com>
2023-11-27 16:46:42 +0700

    messing around with types and functions

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+type Names = [
+  string,
+  number
+]
+
+function greet(name: Names):string {
+  let greeting:string = "";
+    for (let i=0; i<name.length; i++) {
+        greeting += "hello " + name[i] + "\n";
+    }
+    return greeting;
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>GG WP</Text>
+      <Text>{greet(["him", 3])}</Text>
       <StatusBar style="auto" />
     </View>
   );
