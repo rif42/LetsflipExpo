356a4f40b8d0f9a858729ee77f450a87faee88e4
rifky <ee.guy.42@gmail.com>
2023-11-29 04:52:26 +0700

    feature: added virtualized list in feed screen. no reactivity since data is taken from plain json file. can import as state and add reactivity later

--- App.tsx
+++ App.tsx
@@ -1,72 +1,124 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, VirtualizedList } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Fontisto from "@expo/vector-icons/Fontisto";
 import Entypo from "@expo/vector-icons/Entypo";
 import React from "react";
+import { Dimensions } from "react-native";
+import imageData from "./data/imageData.json";
+import feedData from "./lol.json";
+import * as NavigationBar from "expo-navigation-bar";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const getItem = (_data: any, index: number) => _data[index];
+
+const getItemCount = (_data: number) => 5;
+
+const keyExtractor = (item: any) => item.id;
+
+const ImageItem = ({ item }: any) => (
+    <View>
+        <Image
+            source={{
+                uri: item.thumbnail.image_full_path,
+            }}
+            style={styles.feedImg}
+        />
+        <Text style={styles.feedItem}>{item.title}</Text>
+        <Fontisto name='heart-alt' size={28} color='white' style={{ position: "absolute", right: 20, bottom: 20 }} />
+        <Text style={{ position: "absolute", right: 30, bottom: 28, color: "white", fontSize: 12 }}>
+            {item.total_like}
+        </Text>
+        <Fontisto name='comment' size={28} color='white' style={{ position: "absolute", right: 20, bottom: 60 }} />
+        <Text style={{ position: "absolute", right: 33, bottom: 69, color: "white", fontSize: 12 }}>
+            {item.total_comment}
+        </Text>
+    </View>
+);
 
 export default function App() {
+    const visibility = NavigationBar.useVisibility();
+    NavigationBar.setVisibilityAsync("visible");
+
     const [feedSearch, setfeedSearch] = React.useState<boolean>(false);
     const [feedSearchText, setfeedSearchText] = React.useState<string>("");
     const [feedTab, setfeedTab] = React.useState<boolean>(false); // 0 = trending, 1 = recent
 
     return (
-        <SafeAreaView style={styles.container}>
-            <LinearGradient
-                // Background Linear Gradient
-                start={{ x: 0.2, y: 0.2 }}
-                end={{ x: 1, y: 1 }}
-                colors={["navy", "magenta"]}
-                style={styles.background}
-            />
-            <View style={styles.topBar}>
-                <Fontisto
-                    name='search'
-                    size={20}
-                    color='black'
-                    style={styles.searchIcon}
-                    onPress={() => {
-                        setfeedSearch(true);
-                    }}
+        <View style={{ flex: 1, backgroundColor: "black" }}>
+            <SafeAreaView style={styles.container}>
+                <LinearGradient
+                    // Background Linear Gradient
+                    start={{ x: 0.2, y: 0.2 }}
+                    end={{ x: 1, y: 1 }}
+                    colors={["navy", "magenta"]}
+                    style={styles.background}
                 />
-                <Text style={styles.letsFlipFeed}>Let's Flip!</Text>
-                <TextInput
-                    style={feedSearch ? styles.searchTextInput : { display: "none" }}
-                    placeholder='Search'
-                    onChangeText={(text) => setfeedSearchText(text)}
-                    // value={"Search"}
-                />
-                <Entypo
-                    name='cross'
-                    size={28}
-                    color='black'
-                    style={styles.searchIcon}
-                    onPress={() => {
-                        setfeedSearchText("");
-                        setfeedSearch(false);
-                    }}
-                />
-            </View>
-            <View style={styles.feedTabs}>
-                <TouchableOpacity
-                    onPress={() => {
-                        setfeedTab(!feedTab);
-                    }}
-                    style={styles.feedTabItems}>
-                    <Text style={feedTab ? styles.feedTabItemsText : styles.feedTabItemsTextBolded}>Trending</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => {
-                        setfeedTab(!feedTab);
-                    }}
-                    style={styles.feedTabItems}>
-                    <Text style={feedTab ? styles.feedTabItemsTextBolded : styles.feedTabItemsText}>Recent</Text>
-                </TouchableOpacity>
-            </View>
-            <Text>LMAO</Text>
-            <StatusBar style='light' />
-        </SafeAreaView>
+                <View style={styles.topBar}>
+                    <Fontisto
+                        name='search'
+                        size={20}
+                        color='white'
+                        style={styles.searchIcon}
+                        onPress={() => {
+                            setfeedSearch(true);
+                        }}
+                    />
+                    <Text style={styles.letsFlipFeed}>Let's Flip!</Text>
+                    <TextInput
+                        style={feedSearch ? styles.searchTextInput : { display: "none" }}
+                        placeholder='Search'
+                        onChangeText={(text) => setfeedSearchText(text)}
+                        // value={"Search"}
+                    />
+                    <Entypo
+                        name='cross'
+                        size={28}
+                        color='white'
+                        style={styles.searchIcon}
+                        onPress={() => {
+                            setfeedSearchText("");
+                            setfeedSearch(false);
+                        }}
+                    />
+                </View>
+                <View style={styles.feedTabs}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setfeedTab(!feedTab);
+                        }}
+                        style={styles.feedTabItems}>
+                        <Text style={feedTab ? styles.feedTabItemsText : styles.feedTabItemsTextBolded}>Trending</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setfeedTab(!feedTab);
+                        }}
+                        style={styles.feedTabItems}>
+                        <Text style={feedTab ? styles.feedTabItemsTextBolded : styles.feedTabItemsText}>Recent</Text>
+                    </TouchableOpacity>
+                </View>
+                {/* <Text>{feedData[3].id}</Text> */}
+                <View style={styles.imgContainer}>
+                    <VirtualizedList
+                        initialNumToRender={1}
+                        snapToInterval={windowHeight / 1.288}
+                        style={styles.VirtualizedListcontainer}
+                        data={feedData}
+                        getItem={getItem}
+                        getItemCount={getItemCount}
+                        keyExtractor={keyExtractor}
+                        renderItem={({ item }) => <ImageItem item={item} />}
+                        showsVerticalScrollIndicator={false}
+                    />
+                </View>
+
+                <StatusBar style='light' />
+            </SafeAreaView>
+        </View>
     );
 }
 
@@ -94,7 +146,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         width: "100%",
         height: "10%",
-        backgroundColor: "red",
+        // backgroundColor: "red",
         flexDirection: "row",
     },
     searchIcon: {
@@ -104,6 +156,7 @@ const styles = StyleSheet.create({
         display: "flex",
         alignSelf: "center",
         fontSize: 25,
+        color: "white",
     },
     searchTextInput: {
         display: "flex",
@@ -122,16 +175,20 @@ const styles = StyleSheet.create({
         display: "flex",
         width: "100%",
         height: "7.5%",
-        backgroundColor: "blue",
+        // backgroundColor: "blue",
         flexDirection: "row",
         justifyContent: "space-around",
         alignItems: "center",
+        borderTopWidth: 1,
+        borderTopColor: "white",
+        borderBottomWidth:1,
+        borderBottomColor: "white",
     },
     feedTabItems: {
         display: "flex",
         flexGrow: 1,
         height: "100%",
-        backgroundColor: "green",
+        // backgroundColor: "green",
         justifyContent: "center",
         alignItems: "center",
         color: "white",
@@ -145,5 +202,31 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: "900",
         textDecorationLine: "underline",
+    },
+    imgContainer: {
+        display: "flex",
+        width: "100%",
+        height: "77.6%",
+        backgroundColor: "blue",
+        // flexDirection: "column",
+        // justifyContent: "space-around",
+        // alignItems: "center",
+    },
+    VirtualizedListcontainer: {
+        width: "100%",
+        height: "100%",
+    },
+    feedItem: {
+        position: "absolute",
+        fontSize: 28,
+        color: "white",
+        padding: 20,
+        // justifyContent: "space-around",
+        // alignItems: "center",
+    },
+    feedImg: {
+        width: windowWidth,
+        height: windowHeight / 1.288,
+        objectFit: "cover",
     },
 });
